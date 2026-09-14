bd614cbcd6b91ec011ca26139f7dd8e5e948253f
Sanjaya <sanjaya.w@jinasena.com.lk>
2026-09-14 13:54:58 +0530

    Set _description on custom models to match CDB display names

--- models/x_material_request_m_stage.py
+++ models/x_material_request_m_stage.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 
 class XMaterialRequestMStage(models.Model):
     _name = 'x_material_request_m_stage'
-    _description = 'X Material Request M Stage'
+    _description = 'Material Request M Stages'
 
     x_name = fields.Char(string='Stage Name')
     x_studio_sequence = fields.Integer(string='Sequence')
--- models/x_material_request_m_tag.py
+++ models/x_material_request_m_tag.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 
 class XMaterialRequestMTag(models.Model):
     _name = 'x_material_request_m_tag'
-    _description = 'X Material Request M Tag'
+    _description = 'Material Request M Tags'
 
     x_color = fields.Integer(string='Color')
     x_name = fields.Char(string='Name')
